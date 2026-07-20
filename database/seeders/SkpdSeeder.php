37e327dd5d31548b5fa8628cf72c36be72f63649
raihan2030 <raihan.muhammad.bjm@gmail.com>
2026-07-20 10:46:19 +0800

    feat: update SkpdSeeder with new SKPD data and banner paths

--- database/seeders/SkpdSeeder.php
+++ database/seeders/SkpdSeeder.php
@@ -16,9 +16,9 @@ class SkpdSeeder extends Seeder
     {
         $dataSkpd = [
             [
-                'kode_skpd' => 'DISKOMINFOTIK-BJM',
-                'nama_skpd' => 'Dinas Komunikasi, Informatika, dan Statistik Kota Banjarmasin',
-                'banner_path' => 'banners/diskominfo-header.jpg', // Banner di tingkat SKPD
+                'kode_skpd' => '2.16.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Komunikasi, Informatika dan Statistik',
+                'banner_path' => 'banners/diskominfo-header.jpg',
                 'bidang' => [
                     [
                         'nama_bidang' => 'Layanan E-Government & Aplikasi',
@@ -38,9 +38,9 @@ class SkpdSeeder extends Seeder
                 ]
             ],
             [
-                'kode_skpd' => 'DINKES-BJM',
-                'nama_skpd' => 'Dinas Kesehatan Kota Banjarmasin',
-                'banner_path' => 'banners/dinkes-header.jpg', // Banner di tingkat SKPD
+                'kode_skpd' => '1.02.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Kesehatan',
+                'banner_path' => 'banners/dinkes-header.jpg',
                 'bidang' => [
                     [
                         'nama_bidang' => 'Sekretariat & Administrasi Umum',
@@ -54,6 +54,131 @@ class SkpdSeeder extends Seeder
                     ],
                 ]
             ],
+            [
+                'kode_skpd' => '4.01.0.00.0.00.01.0000',
+                'nama_skpd' => 'Sekretariat Daerah',
+                'banner_path' => 'banners/sekda-header.jpg',
+            ],
+            [
+                'kode_skpd' => '4.02.0.00.0.00.01.0000',
+                'nama_skpd' => 'Sekretariat Dewan Perwakilan Rakyat Daerah',
+                'banner_path' => 'banners/setwan-header.jpg',
+            ],
+            [
+                'kode_skpd' => '6.01.0.00.0.00.01.0000',
+                'nama_skpd' => 'Inspektorat Daerah',
+                'banner_path' => 'banners/inspektorat-header.jpg',
+            ],
+            [
+                'kode_skpd' => '5.03.0.00.0.00.01.0000',
+                'nama_skpd' => 'Badan Kepegawaian dan Pengembangan Sumber Daya Manusia',
+                'banner_path' => 'banners/bkpsdm-header.jpg',
+            ],
+            [
+                'kode_skpd' => '5.01.0.00.0.00.01.0000',
+                'nama_skpd' => 'Badan Perencanaan, Penelitian, dan Pengembangan Daerah',
+                'banner_path' => 'banners/bapperinda-header.jpg',
+            ],
+            [
+                'kode_skpd' => '5.02.0.00.0.00.01.0000',
+                'nama_skpd' => 'Badan Pengelolaan Keuangan, Pendapatan dan Aset Daerah',
+                'banner_path' => 'banners/bpkpad-header.jpg',
+            ],
+            [
+                'kode_skpd' => '1.05.0.00.0.00.02.0000',
+                'nama_skpd' => 'Badan Penanggulangan Bencana Daerah',
+                'banner_path' => 'banners/bpbd-header.jpg',
+            ],
+            [
+                'kode_skpd' => '8.01.0.00.0.00.01.0000',
+                'nama_skpd' => 'Badan Kesatuan Bangsa dan Politik',
+                'banner_path' => 'banners/bakesbangpol-header.jpg',
+            ],
+            [
+                'kode_skpd' => '1.01.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Pendidikan',
+                'banner_path' => 'banners/disdik-header.jpg',
+            ],
+            [
+                'kode_skpd' => '1.03.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Pekerjaan Umum dan Penataan Ruang',
+                'banner_path' => 'banners/dpupr-header.jpg',
+            ],
+            [
+                'kode_skpd' => '1.04.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Perumahan Rakyat dan Kawasan Permukiman',
+                'banner_path' => 'banners/disperkim-header.jpg',
+            ],
+            [
+                'kode_skpd' => '1.05.0.00.0.00.01.0000',
+                'nama_skpd' => 'Satuan Polisi Pamong Praja',
+                'banner_path' => 'banners/satpolpp-header.jpg',
+            ],
+            [
+                'kode_skpd' => '1.05.0.00.0.00.03.0000',
+                'nama_skpd' => 'Dinas Pemadam Kebakaran dan Penyelamatan',
+                'banner_path' => 'banners/dpkp-header.jpg',
+            ],
+            [
+                'kode_skpd' => '1.06.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Sosial',
+                'banner_path' => 'banners/dinsos-header.jpg',
+            ],
+            [
+                'kode_skpd' => '2.08.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Pemberdayaan Perempuan dan Perlindungan Anak',
+                'banner_path' => 'banners/dpppa-header.jpg',
+            ],
+            [
+                'kode_skpd' => '2.09.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Ketahanan Pangan, Pertanian dan Perikanan',
+                'banner_path' => 'banners/dkp3-header.jpg',
+            ],
+            [
+                'kode_skpd' => '2.11.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Lingkungan Hidup',
+                'banner_path' => 'banners/dlh-header.jpg',
+            ],
+            [
+                'kode_skpd' => '2.12.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Kependudukan dan Pencatatan Sipil',
+                'banner_path' => 'banners/disdukcapil-header.jpg',
+            ],
+            [
+                'kode_skpd' => '2.13.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Pengendalian Penduduk, Keluarga Berencana dan Pemberdayaan Masyarakat',
+                'banner_path' => 'banners/dppkbpm-header.jpg',
+            ],
+            [
+                'kode_skpd' => '2.15.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Perhubungan',
+                'banner_path' => 'banners/dishub-header.jpg',
+            ],
+            [
+                'kode_skpd' => '2.17.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Koperasi, Usaha Mikro dan Tenaga Kerja',
+                'banner_path' => 'banners/diskopumker-header.jpg',
+            ],
+            [
+                'kode_skpd' => '2.18.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Penanaman Modal dan Pelayanan Terpadu Satu Pintu',
+                'banner_path' => 'banners/dpmptsp-header.jpg',
+            ],
+            [
+                'kode_skpd' => '2.19.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Kebudayaan, Kepemudaan, Olahraga dan Pariwisata',
+                'banner_path' => 'banners/disbudporapar-header.jpg',
+            ],
+            [
+                'kode_skpd' => '2.23.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Perpustakaan dan Kearsipan',
+                'banner_path' => 'banners/dispersip-header.jpg',
+            ],
+            [
+                'kode_skpd' => '3.30.0.00.0.00.01.0000',
+                'nama_skpd' => 'Dinas Perdagangan dan Perindustrian',
+                'banner_path' => 'banners/disperdagin-header.jpg',
+            ],
         ];
 
         foreach ($dataSkpd as $item) {
@@ -61,11 +186,11 @@ class SkpdSeeder extends Seeder
                 ['kode_skpd' => $item['kode_skpd']],
                 [
                     'nama_skpd' => $item['nama_skpd'],
-                    'banner_path' => $item['banner_path']
+                    'banner_path' => $item['banner_path'] ?? null
                 ]
             );
 
-            foreach ($item['bidang'] as $b) {
+            foreach ($item['bidang'] ?? [] as $b) {
                 Bidang::updateOrCreate(
                     [
                         'skpd_id' => $skpd->id,
